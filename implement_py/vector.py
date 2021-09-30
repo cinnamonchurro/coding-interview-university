5519d431a945f668830fa5733656359621ed8f74
Isaac Lew <lewyzisaac@gmail.com>
2021-10-01 00:38:17 +0800

    Update vector.py
    added some descriptor and revised certain functions after further revision of classes

--- implement_py/vector.py
+++ implement_py/vector.py
@@ -1,12 +1,16 @@
 class vector :
-	def __init__(self, name):
-		self.data = []
-		self.name = name
+	""" Trying to implement dynamically 
+	sized array in python for practise"""
+	def __init__(self, capacity = 4):
 		self.size = 0
 		self.capacity = capacity
+		self.array = self.create_array(self.capacity)
+
+	def create_array(self, capacity):
+		return [None] * capacity
 
 	def size(self, size):
-		self.size = size
+		return(self.size)
 
 	def capacity(self, capacity):
 		self.capacity = capacity
@@ -15,49 +19,77 @@ class vector :
 		return(self.size == 0)
 
 	def at(self, idx):
-		if idx < self.size :
-			# return() something
+		if 0 <= idx < self.size :
+			return(self.data[idx])
 			pass
 		else :
-			### Raise an exception!
-			pass 
+			raise IndexError("{0} is out of range! Please specify an index between {1} and {2}".format(idx, 0, self.size))
 
 	def push(self, item):
 		if self.capacity <= self.size :
-			self.capacity *= 2
-		self.append(item)
+			temp_capacity = 2 * self.capacity
+			temp_array = self.create_array(temp_capacity)
+			for i in range(self.size) :
+				temp_array[i] = self.array[i]
+			self.array = temp_array
+			self.capacity = temp_capacity
+		self.array[self.size] = item
 		self.size += 1
 
 	def insert(self, idx, item):
-		if idx > self.size :
-			### Raise an exception!
-			pass
-		else :
-			if self.capacity <= self.size :
-				self.capacity *=2
-			self.size += 1
-			pointer = self.size - 1
-			while pointer > index :
-				self[pointer] = self[pointer -1]
+		if not (0 <= idx < self.size) :
+			raise IndexError("{0} is out of range! Please specify an index between {1} and {2}".format(idx, 0, self.size))
+		elif self.capacity <= self.size :
+			temp_capacity = 2 * self.capacity
+			temp_array = self.create_array(temp_capacity)
+			for i in range(self.size) :
+				temp_array[i] = self.array[i]
+			self.array = temp_array
+			self.capacity = temp_capacity
+			
+			pointer = self.size
+			while pointer > idx :
+				self.array[pointer] = self[pointer -1]
 				pointer -= 1
-			self[idx] = item
+			self.array[idx] = item
+			self.size += 1
 
 	def prepend(self, item):
 		self.insert(0, item)
 
 	def pop(self):
-		self[size-1] = deleted
-		self.size -= 1
+		if self.size > 0 :
+			target = self.array[self.size-1]
+			self.array[self.size-1] = None
+			self.size -= 1
+			if self.size/self.capacity < 0.25:
+				temp_capacity = 0.5 * self.capacity
+				temp_array = self.create_array(temp_capacity)
+				for i in range(self.size):
+					temp_array[i] = self.array[i]
+				self.array = temp_array
+				self.capacity = temp_capacity
+			return(target)
+		else:
+			raise IndexError("There is nothing to pop!")
+
 
 	def delete(self, idx):
-		if idx > self.size :
-			### Raise an exception!
-			pass
+		if not (0 <= idx < self.size) :
+			raise IndexError("{0} is out of range! Please specify an index between {1} and {2}".format(idx, 0, self.size))
 		else :
-			pointer = self.size - 1
-			while idx < pointer :
-				self[idx] = self[idx+1]
-			self[pointer] deleted
+			self.size -= 1
+			while idx < self.size:
+				self.array[idx] = self.array[idx+1]
+				idx += 1
+			self.array[idx] = None
+			if self.size/self.capacity < 0.25:
+				temp_capacity = 0.5 * self.capacity
+				temp_array = self.create_array(temp_capacity)
+				for i in range(self.size):
+					temp_array[i] = self.array[i]
+				self.array = temp_array
+				self.capacity = temp_capacity
 
 	def remove(self, item):
 		- looks for value and removes index holding it (even if in multiple places)
@@ -65,6 +97,6 @@ class vector :
 	def find(self, item):
 		- looks for value and returns first index with that value, -1 if not found
 
-	def resize(self. new_capacity) : // private function
+	def resize(self, new_capacity) : // private function
             - when you reach capacity, resize to double the size
             - when popping an item, if size is 1/4 of capacity, resize to half
